85a1e24bc79a02e2c3a18e354b52b360458a4854
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-05-13 19:56:22 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,13 @@
+import pylab
+import numpy as np
+name = input("What is Your Name? ")
+sur = input("What is your surname? ")
+y = input("What is the Strength of CSE Department? ")
+print (np.pi, "is the value of PI")
+print(np.e, "is the value of e")
+print("PI: %3.3e" %np.pi)
+print("PI: %3s" %np.pi)
+
+print("Your Name is", name, sur,"." )
+print("Strength of CSE Department is", y, ".\n")
+print("Your Name is", name, sur,"and the strength of your department is", y, ".")
