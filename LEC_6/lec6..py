8a37f805202c44fa8894b2ff56b05147db35aa8d
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-05-17 12:10:28 +0530

    Function Calling
    Today we learnt how to call functions in python and some nuances of using them effectively. Quite interesting. But I do feel the need to learn from the Coursera Course on Python Introduction. Some of the next codes will be for the course.

---
+++ LEC_6/lec6..py
@@ -0,0 +1,10 @@
+import pylab
+print("Enter a number \t")
+x = int(input())
+
+if(x < 10):
+    print("Number less than 10\n")
+elif(x == 10):
+    print("NUmber is equal to 10\n")
+else:
+    print("Number is greater than 10")
---
+++ LEC_7/function_2f.py
@@ -0,0 +1,7 @@
+import pylab
+import factorial as f
+
+x = int(input("Enter the 1st Number\t"))
+y = int(input("Enter the 2nd Number\t"))
+
+print("The Sum of Factorial of the 2 Numbers is", f.factorial(x) + f.factorial(y), ".")
