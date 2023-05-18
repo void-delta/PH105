4cacce43c03d9b58460b52a5c60615bc4277a7cb
Delta062004 <22bcs037@iiitdwd.ac.in>
2023-05-18 12:06:59 +0530

    Create file.py
    Understanding file handling in python

---
+++ LEC_8/file.py
@@ -0,0 +1,18 @@
+# looking at file handling in python
+import pylab as pl
+import numpy as np
+import matplotlib as mtplt
+import factorial as f
+
+name = open("new.txt", "w")
+
+for i in range(0, 4):
+    digit = int(input("Enter the Digit\t"))
+    digit_2 = int(input("Enter the Digit\t"))
+    print("\n")
+    name.write("Line %d -> %d %d %d \n" %(i, digit, digit_2, f.factorial(digit)+f.factorial(digit_2)))
+name.close()
+
+nam = open("new.txt", "r")
+string = nam.read()
+print("The Numbers entered are\n\n",string)
